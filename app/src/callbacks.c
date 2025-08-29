4c32f30b4afa66792824c726f945f99e236341c3
Vitali Kailevich <vit.kailev@gmail.com>
2025-08-29 03:13:13 +0200

    Add the UART interface callback functions

--- app/src/callbacks.c
+++ app/src/callbacks.c
@@ -9,8 +9,8 @@
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
     BaseType_t priorityTaskWoken = pdFALSE;
 
-    if (hadc->Instance == ((ADC_HandleTypeDef *) Mcu.adc.handle)->Instance) {
-        xTaskNotifyFromISR(Mcu.jobs.handles[SENSORS_JOB], JOB_NOTIFY_ADC_FLAG, eSetBits, &priorityTaskWoken);
+    if (hadc->Instance == ((ADC_HandleTypeDef *) Application.hardware.adc.handle)->Instance) {
+        xTaskNotifyFromISR(Application.handles[SENSORS_JOB], JOB_NOTIF_SENSOR_FLAG, eSetBits, &priorityTaskWoken);
     }
 
     portYIELD_FROM_ISR(priorityTaskWoken);
@@ -23,8 +23,77 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
 void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *hadc) {
     BaseType_t priorityTaskWoken = pdFALSE;
 
-    if (hadc->Instance == ((ADC_HandleTypeDef *) Mcu.adc.handle)->Instance) {
-        xTaskNotifyFromISR(Mcu.jobs.handles[SENSORS_JOB], JOB_NOTIFY_ADC_ERR_FLAG, eSetBits, &priorityTaskWoken);
+    if (hadc->Instance == ((ADC_HandleTypeDef *) Application.hardware.adc.handle)->Instance) {
+        xTaskNotifyFromISR(Application.handles[SENSORS_JOB], JOB_NOTIF_SENSOR_ERR_FLAG, eSetBits, &priorityTaskWoken);
+    }
+
+    portYIELD_FROM_ISR(priorityTaskWoken);
+}
+
+/**
+ * @brief The transmission callback function
+ * @param huart is the UART handle structure (HAL)
+ */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+    BaseType_t priorityTaskWoken = pdFALSE;
+
+    if (huart->Instance == ((UART_HandleTypeDef *) Serial.uart->handle)->Instance) {
+        xTaskNotifyFromISR(Application.handles[SERIAL_PORT_JOB], SERIAL_NOTIF_TX_FLAG, eSetBits, &priorityTaskWoken);
+    }
+
+    portYIELD_FROM_ISR(priorityTaskWoken);
+}
+
+/**
+ * @brief The reception callback function (Rx event notification called after use of advanced reception service)
+ * @param huart is the UART handle structure (HAL)
+ * @param Size is the number of data available in application reception buffer
+ */
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
+    BaseType_t priorityTaskWoken = pdFALSE;
+
+    if (huart->Instance == ((UART_HandleTypeDef *) Serial.uart->handle)->Instance) {
+        switch (huart->RxEventType) {
+            case HAL_UART_RXEVENT_TC:
+                break;
+            case HAL_UART_RXEVENT_HT:
+                break;
+            case HAL_UART_RXEVENT_IDLE:
+                xStreamBufferSendFromISR(Serial.rxStream, Serial.rxBuffer, Size, &priorityTaskWoken);
+                xTaskNotifyFromISR(Application.handles[SERIAL_PORT_JOB], SERIAL_NOTIF_RX_FLAG, eSetBits,
+                                   &priorityTaskWoken);
+                break;
+            default:
+                break;
+        }
+    }
+
+    portYIELD_FROM_ISR(priorityTaskWoken);
+}
+
+/**
+ * @brief The UART error callback function
+ * @param huart is the UART handle structure (HAL)
+ */
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart) {
+    BaseType_t priorityTaskWoken = pdFALSE;
+
+    if (huart->Instance == ((UART_HandleTypeDef *) Serial.uart->handle)->Instance) {
+        xTaskNotifyFromISR(Application.handles[SERIAL_PORT_JOB], SERIAL_NOTIF_ERR_FLAG, eSetBits, &priorityTaskWoken);
+    }
+
+    portYIELD_FROM_ISR(priorityTaskWoken);
+}
+
+/**
+ * @brief The UART abort complete callback function
+ * @param huart is the UART handle structure (HAL)
+ */
+void HAL_UART_AbortCpltCallback(UART_HandleTypeDef *huart) {
+    BaseType_t priorityTaskWoken = pdFALSE;
+
+    if (huart->Instance == ((UART_HandleTypeDef *) Serial.uart->handle)->Instance) {
+        xTaskNotifyFromISR(Application.handles[SERIAL_PORT_JOB], SERIAL_NOTIF_ABORT_FLAG, eSetBits, &priorityTaskWoken);
     }
 
     portYIELD_FROM_ISR(priorityTaskWoken);
